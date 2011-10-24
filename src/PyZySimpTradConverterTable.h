e7e318308e5d830a9d32796addeefebd1af263c5
Hiroshi Sumita <hsumita@chromium.org>
2011-10-24 15:59:19 +0900

    Change the license from GPL to LGPL.
    This CL don't modify files under debian/*.
    Some files which is removed by http://codereview.appspot.com/5305041/ are also unmodified.
    
    BUG=None
    TEST=Manually
    
    Review URL: http://codereview.appspot.com/5301043

--- src/PyZySimpTradConverterTable.h
+++ src/PyZySimpTradConverterTable.h
@@ -1,3 +1,24 @@
+/* vim:set et ts=4 sts=4:
+ *
+ * libpyzy - The Chinese PinYin and Bopomofo conversion library.
+ *
+ * Copyright (c) 2008-2010 Peng Huang <shawn.p.huang@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
 static const gchar *simp_to_trad[][2] = {
     { "〇余", "〇餘" },
     { "〇只", "〇隻" },
